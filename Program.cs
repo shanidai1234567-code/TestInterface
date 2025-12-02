ae61e8bf22a4851d8c1abce9d3203d164ceb0b26
shanidai1234567-code <shanidai1234567@gmail.com>
2025-12-02 12:26:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ICloseToHelp;
+using WebApplication1;
+
+namespace TestInterface
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            MyApi myApi = new MyApi();
+         //   myApi.UpdateACity("New York");
+
+        }
+    }
+}
